b1203969d62b3720325d81fa98da4daef592449b
RiyazSyed <riyazsyed.8790@gmail.com>
2023-09-24 00:20:18 +0530

    Page1 validation done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import React from 'react';
+import Page1 from './Components/Page1';
 function App() {
   const [formdata,setData] = React.useState([{
     name:"",
@@ -9,6 +10,7 @@ function App() {
     fiscal:1,
     addOns:[1,2,3]
   }]);
+  const [miss,setMiss] = React.useState([]);
   function formHandler(e){
     const {name,value} = e.target;
         setData((prev)=>{
@@ -20,6 +22,29 @@ function App() {
           })
         })
       
+  }
+  function validator(){
+    formdata.map((each)=>{
+      let splicer = each.email.split("");
+      if(each.name.trim()==="")
+      {setMiss([1]);}
+      else if(each.email==="")
+      {setMiss([2]);}
+      else if(each.phone==="")
+      {setMiss([3]);}
+      else if(!splicer.includes("@")){
+        setMiss([0]);
+        alert("Invalid email!")
+      }
+      else if(isNaN(each.phone)){
+        setMiss([0]);
+        alert("Invalid number!");
+      }
+      else{
+        alert("Next page cums");
+      }
+      return each;
+    }) 
   }
   return (
     <main className="App bg-[#EEF5FF] ubu w-screen h-screen flex justify-center md:items-center">
@@ -59,29 +84,7 @@ function App() {
             </li>
           </ul>
         </nav>
-        <section className='rounded-xl px-4 py-4 md:px-0 md:py-0 flex justify-center text-left info w-4/5 h-4/5 md:h-full z-10 bg-white'>
-          <div className='flex flex-col justify-between h-full md:justify-evenly '>
-            <div>
-            <h2 className='tracking-wide text-[#012257] font-semibold text-3xl '>Personal Info</h2>
-            <h3 className=' text-gray-500 text-base'>Please provide your name, email address, and phone number. </h3>
-            </div>
-            <div>
-              <label htmlFor="name" className=' text-[#48546D]  text-base'>Name</label><br/>
-              <input id="name" name='name' value={formdata.name}  onChange={(e)=>formHandler(e)} className=' text-[#012257] font-semibold  outline-blue-800 grayBord w-full rounded-md px-2 py-2' type='text' placeholder='e.g. Stephen King' required />
-            </div>
-            <div>
-              <label htmlFor="mail" className=' text-[#48546D]  text-base'>Email Address</label><br/>
-              <input id="mail" name="email" value={formdata.email} onChange={(e)=>formHandler(e)}  className=' text-[#012257] font-semibold outline-blue-800 grayBord w-full rounded-md px-2 py-2' type='email' placeholder='e.g. Stephenking@lorem.com' required />
-            </div>
-            <div>
-              <label htmlFor="number" className=' text-[#48546D]  text-base'>Phone Number</label><br/>
-              <input id="number" name="phone" value={formdata.phone} onChange={(e)=>formHandler(e)}  className=' text-[#012257] font-semibold outline-blue-800 grayBord w-full rounded-md px-2 py-2' type='tel' placeholder='e.g. +1 234 567 890' required />
-            </div>
-            <div className=' flex justify-center md:justify-end'>
-              <button className=' text-white h-12 w-24 rounded-xl bg-[#052859]'>Next Step</button>
-            </div>
-          </div>
-        </section>
+        <Page1 click={validator} miss={miss} change={formHandler} name_val={formdata[0].name} email_val={formdata[0].email} phone_val={formdata[0].phone} />
       </section>
     </main>
   );
---
+++ src/Components/Page1.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+export default function Page1(props){
+    return(
+        <section className='rounded-xl px-4 py-4 md:px-0 md:py-0 flex justify-center text-left info w-4/5 h-4/5 md:h-full z-10 bg-white'>
+          <div className='flex flex-col justify-between h-full md:justify-evenly '>
+            <div>
+            <h2 className='tracking-wide text-[#012257] font-semibold text-3xl '>Personal Info</h2>
+            <h3 className=' text-gray-500 text-base'>Please provide your name, email address, and phone number. </h3>
+            </div>
+            <div>
+              <div className=' flex w-full justify-between'>
+              <label htmlFor="name" className=' text-[#48546D]  text-base'>Name</label>
+              <label style={props.miss.includes(1)?{display:"block"}:{}} htmlFor="name"  className=' text-red-500 hidden  text-base'>This field is required</label>
+              </div>
+              <input id="name" name='name' value={props.name_val}  onChange={(e)=>props.change(e)} className=' text-[#012257] focus:font-semibold  outline-blue-800 grayBord w-full rounded-md px-2 py-2' type='text' placeholder='e.g. Stephen King' required />
+            </div>
+            <div>
+              <div className=' flex w-full justify-between'>
+              <label htmlFor="mail" className=' text-[#48546D]  text-base'>Email Address</label>
+              <label style={props.miss.includes(2)?{display:"block"}:{}} htmlFor="email" className=' text-red-500 hidden  text-base'>This field is required</label>
+              </div>
+              <input id="mail" name="email" value={props.email_val} onChange={(e)=>props.change(e)}  className=' text-[#012257] focus:font-semibold outline-blue-800 grayBord w-full rounded-md px-2 py-2' type='email' placeholder='e.g. Stephenking@lorem.com' required />
+            </div>
+            <div>
+              <div className=' flex w-full justify-between'>
+              <label htmlFor="number" className=' text-[#48546D]  text-base'>Phone Number</label>
+              <label style={props.miss.includes(3)?{display:"block"}:{}} htmlFor="number" className=' text-red-500 hidden  text-base'>This field is required</label>
+              </div>
+              <input id="number" name="phone" value={props.phone_val} onChange={(e)=>props.change(e)}  className=' text-[#012257] focus:font-semibold outline-blue-800 grayBord w-full rounded-md px-2 py-2' type='tel' placeholder='e.g. +1 234 567 890' required />
+            </div>
+            <div className=' flex justify-center md:justify-end'>
+              <button onClick={props.click} className=' text-white h-12 w-24 rounded-xl bg-[#052859]'>Next Step</button>
+            </div>
+          </div>
+        </section>
+    )
+}
