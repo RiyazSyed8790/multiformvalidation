6c96479dd7176062c9957767aedfcd64a2a7a303
RiyazSyed <riyazsyed.8790@gmail.com>
2023-09-23 21:06:19 +0530

    Added form state manager

--- src/App.js
+++ src/App.js
@@ -1,6 +1,26 @@
 import './App.css';
-
+import React from 'react';
 function App() {
+  const [formdata,setData] = React.useState([{
+    name:"",
+    email:"",
+    phone:"",
+    tier:1,
+    fiscal:1,
+    addOns:[1,2,3]
+  }]);
+  function formHandler(e){
+    const {name,value} = e.target;
+        setData((prev)=>{
+         return prev.map((each)=>{
+            return {
+              ...each,
+              [name]:value
+            }
+          })
+        })
+      
+  }
   return (
     <main className="App bg-[#EEF5FF] ubu w-screen h-screen flex justify-center md:items-center">
       <section className=' bg-[#EEF5FF] md:bg-white rounded-xl flex justify-center items-end md:items-start flex-wrap md:flex-nowrap md:w-2/3 w-full h-full md:h-2/3'>
@@ -39,26 +59,24 @@ function App() {
             </li>
           </ul>
         </nav>
-        <section className='rounded-xl px-2 py-2 md:px-0 md:py-0 flex justify-center text-left info w-3/4 h-4/5 md:h-full z-10 bg-white'>
-          <div className='flex flex-col justify-between md:justify-evenly '>
+        <section className='rounded-xl px-4 py-4 md:px-0 md:py-0 flex justify-center text-left info w-4/5 h-4/5 md:h-full z-10 bg-white'>
+          <div className='flex flex-col justify-between h-full md:justify-evenly '>
             <div>
             <h2 className='tracking-wide text-[#012257] font-semibold text-3xl '>Personal Info</h2>
             <h3 className=' text-gray-500 text-base'>Please provide your name, email address, and phone number. </h3>
             </div>
-            
-          <div>
-              <label className=' text-[#48546D]  text-base'>Name</label><br/>
-              <input className=' text-[#012257] font-semibold  outline-blue-800 grayBord w-full rounded-md px-2 py-2' type='text' placeholder='e.g. Stephen King' required />
+            <div>
+              <label htmlFor="name" className=' text-[#48546D]  text-base'>Name</label><br/>
+              <input id="name" name='name' value={formdata.name}  onChange={(e)=>formHandler(e)} className=' text-[#012257] font-semibold  outline-blue-800 grayBord w-full rounded-md px-2 py-2' type='text' placeholder='e.g. Stephen King' required />
             </div>
             <div>
-              <label className=' text-[#48546D]  text-base'>Email Address</label><br/>
-              <input className=' text-[#012257] font-semibold outline-blue-800 grayBord w-full rounded-md px-2 py-2' type='email' placeholder='e.g. Stephenking@lorem.com' required />
+              <label htmlFor="mail" className=' text-[#48546D]  text-base'>Email Address</label><br/>
+              <input id="mail" name="email" value={formdata.email} onChange={(e)=>formHandler(e)}  className=' text-[#012257] font-semibold outline-blue-800 grayBord w-full rounded-md px-2 py-2' type='email' placeholder='e.g. Stephenking@lorem.com' required />
             </div>
             <div>
-              <label className=' text-[#48546D]  text-base'>Phone Number</label><br/>
-              <input className=' text-[#012257] font-semibold outline-blue-800 grayBord w-full rounded-md px-2 py-2' type='tel' placeholder='e.g. +1 234 567 890' required />
+              <label htmlFor="number" className=' text-[#48546D]  text-base'>Phone Number</label><br/>
+              <input id="number" name="phone" value={formdata.phone} onChange={(e)=>formHandler(e)}  className=' text-[#012257] font-semibold outline-blue-800 grayBord w-full rounded-md px-2 py-2' type='tel' placeholder='e.g. +1 234 567 890' required />
             </div>
-            
             <div className=' flex justify-center md:justify-end'>
               <button className=' text-white h-12 w-24 rounded-xl bg-[#052859]'>Next Step</button>
             </div>
