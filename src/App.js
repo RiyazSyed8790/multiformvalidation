210d1b6b96d7855b71eace3c53e079c8161752bf
RiyazSyed <riyazsyed.8790@gmail.com>
2023-09-24 19:28:18 +0530

    Page 2 complete

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import React from 'react';
 import Page1 from './Components/Page1';
 import Page2 from './Components/Page2';
+import Navbar from './Components/Navbar';
 function App() {
   const [formdata,setData] = React.useState([{
     name:"",
@@ -24,6 +25,19 @@ function App() {
           })
         })
       
+  }
+  function tierHandler(e){
+    console.log(e);
+    
+      setData((prev)=>{
+        return prev.map((each)=>{
+          return {
+            ...each,
+            tier:e
+          }
+        })
+      })
+    
   }
   function validator(){
     formdata.map((each)=>{
@@ -54,15 +68,15 @@ function App() {
   }
   const arr = [
     <Page1 click={validator} miss={miss} change={formHandler} name_val={formdata[0].name} email_val={formdata[0].email} phone_val={formdata[0].phone} />,
-    <Page2 back={goBack} click={validator} />
+    <Page2 back={goBack} click={validator} tier={tierHandler} active={formdata[0].tier} />
   ]
   return (
     <main className="App bg-[#EEF5FF] ubu w-screen h-screen flex justify-center md:items-center">
       <section className=' bg-[#EEF5FF] md:bg-white rounded-xl flex justify-center items-end md:items-start flex-wrap md:flex-nowrap md:w-2/3 w-full h-full md:h-2/3'>
-        <nav className=' mob md:desk w-full md:w-1/4  h-1/3 md:h-[96%] flex justify-center items-start md:justify-start  md:rounded-xl md:my-2 py-12 md:py-0 md:mx-2 absolute top-0 md:static   '>
+        {/* <nav className=' mob md:desk w-full md:w-1/4  h-1/3 md:h-[96%] flex justify-center items-start md:justify-start  md:rounded-xl md:my-2 py-12 md:py-0 md:mx-2 absolute top-0 md:static   '>
           <ul className=' flex md:block list-none md:child:my-6 md:child:mx-6 child:mx-2'>
             <li className=' flex items-center tracking-wider'>
-              <button className='  w-8 h-8  bg-[#C2E6FF] text-[#05274D] font-semibold rounded-2xl'>1</button>
+              <button style={act===1?{backgroundColor:"#C2E6FF",color:"#05274D"}:{backgroundColor:"transparent",color:"white"}} className='  w-8 h-8 whiteBord  font-semibold rounded-2xl'>1</button>
               <div className='  hidden  md:block text-left ml-4'>
                 <h3 className=' text-sm   text-[#a7a5f1]'>STEP 1</h3>
                 <h2 className=' text-base leading-4  text-white'>YOUR INFO</h2>
@@ -70,7 +84,7 @@ function App() {
             </li>
 
             <li className=' flex items-center'>
-              <button className=' whiteBord w-8 h-8 bg-transparent text-white rounded-2xl'>2</button>
+              <button style={act===2?{backgroundColor:"#C2E6FF",color:"#05274D"}:{backgroundColor:"transparent",color:"white"}} className=' whiteBord w-8 h-8  font-semibold  rounded-2xl'>2</button>
               <div className='  hidden md:block text-left ml-4'>
                 <h3 className=' text-sm   text-[#a7a5f1]'>STEP 2</h3>
                 <h2 className=' text-base leading-4  text-white'>SELECT PLAN</h2>
@@ -78,7 +92,7 @@ function App() {
             </li>
 
             <li className=' flex items-center'>
-              <button className=' whiteBord w-8 h-8 bg-transparent text-white rounded-2xl'>3</button>
+              <button style={act===3?{backgroundColor:"#C2E6FF",color:"#05274D"}:{backgroundColor:"transparent",color:"white"}} className=' whiteBord w-8 h-8  font-semibold  rounded-2xl'>3</button>
               <div className='  hidden md:block text-left ml-4'>
                 <h3 className=' text-sm   text-[#a7a5f1]'>STEP 3</h3>
                 <h2 className=' text-base leading-4  text-white'>ADD-ON</h2>
@@ -86,14 +100,15 @@ function App() {
             </li>
 
             <li className=' flex items-center'>
-              <button className=' whiteBord w-8 h-8 bg-transparent text-white rounded-2xl'>4</button>
+              <button style={act===4?{backgroundColor:"#C2E6FF",color:"#05274D"}:{backgroundColor:"transparent",color:"white"}} className=' whiteBord w-8 h-8  font-semibold  rounded-2xl'>4</button>
               <div className='  hidden md:block text-left ml-4'>
                 <h3 className=' text-sm   text-[#a7a5f1]'>STEP 4</h3>
                 <h2 className=' text-base leading-4  text-white'>SUMMARY</h2>
               </div>
             </li>
           </ul>
-        </nav>
+        </nav> */}
+        <Navbar active={act} />
         {/* <Page1 click={validator} miss={miss} change={formHandler} name_val={formdata[0].name} email_val={formdata[0].email} phone_val={formdata[0].phone} /> */}
         {arr[act-1]}
       </section>
