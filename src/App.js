d7e91d930847a4c57bf38ca253e4249df288a16c
RiyazSyed <riyazsyed.8790@gmail.com>
2023-09-24 19:42:54 +0530

    Added number navigation

--- src/App.js
+++ src/App.js
@@ -27,8 +27,6 @@ function App() {
       
   }
   function tierHandler(e){
-    console.log(e);
-    
       setData((prev)=>{
         return prev.map((each)=>{
           return {
@@ -39,6 +37,7 @@ function App() {
       })
     
   }
+  console.log(act);
   function validator(){
     formdata.map((each)=>{
       let splicer = each.email.split("");
@@ -66,6 +65,9 @@ function App() {
   function goBack(){
     setAct(prev=>prev-1);
   }
+  function navigator(e){
+    setAct(e);
+  }
   const arr = [
     <Page1 click={validator} miss={miss} change={formHandler} name_val={formdata[0].name} email_val={formdata[0].email} phone_val={formdata[0].phone} />,
     <Page2 back={goBack} click={validator} tier={tierHandler} active={formdata[0].tier} />
@@ -73,43 +75,7 @@ function App() {
   return (
     <main className="App bg-[#EEF5FF] ubu w-screen h-screen flex justify-center md:items-center">
       <section className=' bg-[#EEF5FF] md:bg-white rounded-xl flex justify-center items-end md:items-start flex-wrap md:flex-nowrap md:w-2/3 w-full h-full md:h-2/3'>
-        {/* <nav className=' mob md:desk w-full md:w-1/4  h-1/3 md:h-[96%] flex justify-center items-start md:justify-start  md:rounded-xl md:my-2 py-12 md:py-0 md:mx-2 absolute top-0 md:static   '>
-          <ul className=' flex md:block list-none md:child:my-6 md:child:mx-6 child:mx-2'>
-            <li className=' flex items-center tracking-wider'>
-              <button style={act===1?{backgroundColor:"#C2E6FF",color:"#05274D"}:{backgroundColor:"transparent",color:"white"}} className='  w-8 h-8 whiteBord  font-semibold rounded-2xl'>1</button>
-              <div className='  hidden  md:block text-left ml-4'>
-                <h3 className=' text-sm   text-[#a7a5f1]'>STEP 1</h3>
-                <h2 className=' text-base leading-4  text-white'>YOUR INFO</h2>
-              </div>
-            </li>
-
-            <li className=' flex items-center'>
-              <button style={act===2?{backgroundColor:"#C2E6FF",color:"#05274D"}:{backgroundColor:"transparent",color:"white"}} className=' whiteBord w-8 h-8  font-semibold  rounded-2xl'>2</button>
-              <div className='  hidden md:block text-left ml-4'>
-                <h3 className=' text-sm   text-[#a7a5f1]'>STEP 2</h3>
-                <h2 className=' text-base leading-4  text-white'>SELECT PLAN</h2>
-              </div>
-            </li>
-
-            <li className=' flex items-center'>
-              <button style={act===3?{backgroundColor:"#C2E6FF",color:"#05274D"}:{backgroundColor:"transparent",color:"white"}} className=' whiteBord w-8 h-8  font-semibold  rounded-2xl'>3</button>
-              <div className='  hidden md:block text-left ml-4'>
-                <h3 className=' text-sm   text-[#a7a5f1]'>STEP 3</h3>
-                <h2 className=' text-base leading-4  text-white'>ADD-ON</h2>
-              </div>
-            </li>
-
-            <li className=' flex items-center'>
-              <button style={act===4?{backgroundColor:"#C2E6FF",color:"#05274D"}:{backgroundColor:"transparent",color:"white"}} className=' whiteBord w-8 h-8  font-semibold  rounded-2xl'>4</button>
-              <div className='  hidden md:block text-left ml-4'>
-                <h3 className=' text-sm   text-[#a7a5f1]'>STEP 4</h3>
-                <h2 className=' text-base leading-4  text-white'>SUMMARY</h2>
-              </div>
-            </li>
-          </ul>
-        </nav> */}
-        <Navbar active={act} />
-        {/* <Page1 click={validator} miss={miss} change={formHandler} name_val={formdata[0].name} email_val={formdata[0].email} phone_val={formdata[0].phone} /> */}
+        <Navbar active={act} click={navigator} />
         {arr[act-1]}
       </section>
     </main>
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,7 +4,7 @@ export default function Navbar(props){
         <nav className=' mob md:desk w-full md:w-1/4  h-1/3 md:h-[96%] flex justify-center items-start md:justify-start  md:rounded-xl md:my-2 py-12 md:py-0 md:mx-2 absolute top-0 md:static   '>
           <ul className=' flex md:block list-none md:child:my-6 md:child:mx-6 child:mx-2'>
             <li className=' flex items-center tracking-wider'>
-              <button style={props.active===1?{backgroundColor:"#C2E6FF",color:"#05274D"}:{backgroundColor:"transparent",color:"white"}} className='  w-8 h-8 whiteBord  font-semibold rounded-2xl'>1</button>
+              <button style={props.active===1?{backgroundColor:"#C2E6FF",color:"#05274D"}:{backgroundColor:"transparent",color:"white"}} onClick={()=>props.click(1)} className='  w-8 h-8 whiteBord  font-semibold rounded-2xl'>1</button>
               <div className='  hidden  md:block text-left ml-4'>
                 <h3 className=' text-sm   text-[#a7a5f1]'>STEP 1</h3>
                 <h2 className=' text-base leading-4  text-white'>YOUR INFO</h2>
@@ -12,7 +12,7 @@ export default function Navbar(props){
             </li>
 
             <li className=' flex items-center'>
-              <button style={props.active===2?{backgroundColor:"#C2E6FF",color:"#05274D"}:{backgroundColor:"transparent",color:"white"}} className=' whiteBord w-8 h-8  font-semibold  rounded-2xl'>2</button>
+              <button style={props.active===2?{backgroundColor:"#C2E6FF",color:"#05274D"}:{backgroundColor:"transparent",color:"white"}} onClick={()=>props.click(2)} className=' whiteBord w-8 h-8  font-semibold  rounded-2xl'>2</button>
               <div className='  hidden md:block text-left ml-4'>
                 <h3 className=' text-sm   text-[#a7a5f1]'>STEP 2</h3>
                 <h2 className=' text-base leading-4  text-white'>SELECT PLAN</h2>
@@ -20,7 +20,7 @@ export default function Navbar(props){
             </li>
 
             <li className=' flex items-center'>
-              <button style={props.active===3?{backgroundColor:"#C2E6FF",color:"#05274D"}:{backgroundColor:"transparent",color:"white"}} className=' whiteBord w-8 h-8  font-semibold  rounded-2xl'>3</button>
+              <button style={props.active===3?{backgroundColor:"#C2E6FF",color:"#05274D"}:{backgroundColor:"transparent",color:"white"}} onClick={()=>props.click(3)} className=' whiteBord w-8 h-8  font-semibold  rounded-2xl'>3</button>
               <div className='  hidden md:block text-left ml-4'>
                 <h3 className=' text-sm   text-[#a7a5f1]'>STEP 3</h3>
                 <h2 className=' text-base leading-4  text-white'>ADD-ON</h2>
@@ -28,7 +28,7 @@ export default function Navbar(props){
             </li>
 
             <li className=' flex items-center'>
-              <button style={props.active===4?{backgroundColor:"#C2E6FF",color:"#05274D"}:{backgroundColor:"transparent",color:"white"}} className=' whiteBord w-8 h-8  font-semibold  rounded-2xl'>4</button>
+              <button style={props.active===4?{backgroundColor:"#C2E6FF",color:"#05274D"}:{backgroundColor:"transparent",color:"white"}} onClick={()=>props.click(4)} className=' whiteBord w-8 h-8  font-semibold  rounded-2xl'>4</button>
               <div className='  hidden md:block text-left ml-4'>
                 <h3 className=' text-sm   text-[#a7a5f1]'>STEP 4</h3>
                 <h2 className=' text-base leading-4  text-white'>SUMMARY</h2>
